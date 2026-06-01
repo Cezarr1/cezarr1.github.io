271006361d06c12f7e6b260a970db79a395edbfe
Cezarr1 <87286428+Cezarr1@users.noreply.github.com>
2026-06-01 20:16:53 +0200

    Update menu.js

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -3,3 +3,9 @@ fetch('/menu/')
 	.then(html => {
 		document.getElementById('menu-container').innerHTML = html;
 });
+
+fetch('/menu_mobile/')
+	.then(r => r.text())
+	.then(html => {
+		document.getElementById('menu-mobile-container').innerHTML = html;
+});
