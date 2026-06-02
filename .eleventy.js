003f7f593d27704bd3c70a0bff4c4c12a7e42264
Cezarr1 <87286428+Cezarr1@users.noreply.github.com>
2026-06-02 23:52:04 +0200

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(eleventyConfig) {
   eleventyConfig.addPassthroughCopy("assets");
   eleventyConfig.addPassthroughCopy("**/*.{jpg,jpeg,png,gif,webp,tif,tiff,jfif,bmp,svg,ico}");
+  eleventyConfig.addPassthroughCopy("**/*.{txt}");
   return {
     dir: {
       output: "_site"
